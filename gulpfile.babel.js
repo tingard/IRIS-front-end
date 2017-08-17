1fc5f6276d05603d7e058ad65071cbda958feb0e
DavidT3 <dt237@sussex.ac.uk>
2017-08-17 22:28:13 +0100

    sass should now be fully integrated. Still getting linting errors in App.spec.jsx

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,10 +5,13 @@ import babel from 'gulp-babel';
 import del from 'del';
 import eslint from 'gulp-eslint';
 import webpack from 'webpack-stream';
+import sass from 'gulp-sass';
 import webpackConfig from './webpack.config.babel';
+
 
 const paths = {
   allSrcJs: 'src/**/*.js?(x)',
+  allSrcScss: 'sass/**/*.scss',
   serverSrcJs: 'src/server/**/*.js?(x)',
   sharedSrcJs: 'src/shared/**/*.js?(x)',
   clientEntryPoint: 'src/client/index.jsx',
@@ -41,14 +44,21 @@ gulp.task('build', ['lint', 'clean'], () =>
     .pipe(gulp.dest(paths.libDir)),
 );
 
-gulp.task('main', ['lint', 'clean'], () =>
-    gulp.src(paths.clientEntryPoint)
-      .pipe(webpack(webpackConfig))
-      .pipe(gulp.dest(paths.distDir)),
+gulp.task('main', ['lint', 'clean', 'sass-styles'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir)),
 );
+
+gulp.task('sass-styles', () => {
+  gulp.src(paths.allSrcScss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
+});
 
 gulp.task('watch', () => {
   gulp.watch(paths.allSrcJs, ['main']);
+  gulp.watch([paths.allSrcJs, paths.allSrcScss], ['main']);
 });
 
 gulp.task('default', ['watch', 'main']);
--- src/client/components/App.spec.jsx
+++ src/client/components/App.spec.jsx
@@ -1,9 +1,10 @@
-/* eslint prefer-arrow-callback: 0, func-names: 0, 'react/jsx-boolean-value': ['error', 'always'], 'react/jsx-filename-extension': 0 */
+/* eslint prefer-arrow-callback: 0, func-names: 0, 'react/comjsx-boolean-value': ['error', 'always'], 'react/jsx-filename-extension': 0 */
 /* global describe, it, beforeEach */
+
 import { mount } from 'enzyme';
 import React from 'react';
 import assert from 'assert';
-import App from './App'
+import App from './App';
 
 describe('App', function () {
   let wrapper;
