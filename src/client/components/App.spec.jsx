a04b1b96e7df706b59ad92035c618850205c4029
DavidT3 <dt237@sussex.ac.uk>
2017-08-22 10:47:08 +0100

    Changed comjsx back to jsx
    Fixed this weird change that I don't remember doing

--- src/client/components/App.spec.jsx
+++ src/client/components/App.spec.jsx
@@ -1,4 +1,4 @@
-/* eslint prefer-arrow-callback: 0, func-names: 0, 'react/comjsx-boolean-value': ['error', 'always'], 'react/jsx-filename-extension': 0 */
+/* eslint prefer-arrow-callback: 0, func-names: 0, 'react/jsx-boolean-value': ['error', 'always'], 'react/jsx-filename-extension': 0 */
 /* global describe, it, beforeEach */
 
 import { mount } from 'enzyme';
