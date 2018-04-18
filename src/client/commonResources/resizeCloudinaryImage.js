9ba6979a103cb93684656903a108f849f8169c9d
Tim Lingard <tklingard@gmail.com>
2018-04-18 18:00:36 +0100

    Fixed images too tall for image card

--- src/client/commonResources/resizeCloudinaryImage.js
+++ src/client/commonResources/resizeCloudinaryImage.js
@@ -1,7 +1,6 @@
 function resizeCloudinaryImage(url, width) {
   const regExp = /res.cloudinary.com\/(.*?)\/image\/upload\/(.*?)$/i;
   const match = regExp.exec(url);
-  console.log('here', url, match);
   return match ? (
     `https://res.cloudinary.com/${match[1]}/c_scale,w_${width}/${match[2]}`
   ) : url;
