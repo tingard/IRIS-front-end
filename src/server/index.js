daed6ab4e3f0b75c4ac80064202fcd033b4d1f6a
Tim Lingard <tklingard@gmail.com>
2018-04-23 19:55:30 +0100

    Template index.html to send different style/client-bundle depending on route. Halfway house before server-side rendering

--- src/server/index.js
+++ src/server/index.js
@@ -32,20 +32,32 @@ app.get(/\/.*?-bundle\.js(\.map)?\.gz/, (req, res, next) => {
   next();
 });
 
-app.get('/iris', (req, res) => {
-  res.render('index', { clientBundleSource: 'iris-bundle.js' });
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    { clientBundleSource: 'main-bundle.js', styleSource: '/styles/main.css' },
+  );
 });
 
 app.get('/student', (req, res) => {
-  res.render('index', { clientBundleSource: 'student-bundle.js' });
+  res.render(
+    'index',
+    { clientBundleSource: 'student-bundle.js', styleSource: '/styles/student.css' },
+  );
 });
 
 app.get('/volunteer', (req, res) => {
-  res.render('index', { clientBundleSource: 'volunteer-bundle.js' });
+  res.render(
+    'index',
+    { clientBundleSource: 'volunteer-bundle.js', styleSource: '/styles/volunteer.css' },
+  );
 });
 
 app.get('/licence-owner', (req, res) => {
-  res.render('index', { clientBundleSource: 'licence-owner-bundle.js' });
+  res.render(
+    'index',
+    { clientBundleSource: 'licence-owner-bundle.js', styleSource: '/styles/licence-owner.css' },
+  );
 });
 
 app.use(express.static(path.join(__dirname, '../../dist')));
