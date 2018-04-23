f9529cd2150e925c8683495dc0c25e5c1740c911
Tim Lingard <tklingard@gmail.com>
2018-04-23 22:14:22 +0100

    Beginning to flesh out homepage

--- src/client/main/components/HomePage.jsx
+++ src/client/main/components/HomePage.jsx
@@ -3,15 +3,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => (
-  <div id="iris-home-page" className="iris-narrow-page">
-    <h1>Welcome to IRIS</h1>
-    <p>This is going to be fun</p>
-    <Link
-      className="iris-button action"
-      to="/login"
-    >
-      Login
-    </Link>
+  <div id="iris-home-page">
+    <div className="iris-narrow-page">
+      <div style={{ width: '100%', height: '100px' }} />
+      <h1>A picture is worth a thousand words</h1>
+      <p>
+        Let's make IRIS worth ten thousand pictures.
+      </p>
+      <Link
+        className="iris-button action"
+        to="/login"
+      >
+        Login
+      </Link>
+      <Link
+        className="iris-button secondary"
+        to="/login"
+      >
+        Sign up to IRIS
+      </Link>
+      <div style={{ width: '100%', height: '200px' }} />
+      <h2>What is IRIS?</h2>
+      <div className="image-panel">
+        <p>
+          IRIS is an application which connects blind and visually impaired students
+          to a network of volunteers with experience in different subject areas.
+        </p>
+      </div>
+      <div className="image-panel">
+        <p>
+          A student uploads an image from their course, and volunteers provide a
+          subject-specific description of that image.
+        </p>
+      </div>
+    </div>
   </div>
 );
 
