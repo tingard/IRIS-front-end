e21f208498c34ad2833cef91d5c7180826c525b4
Tim Lingard <tklingard@gmail.com>
2018-04-24 14:00:27 +0100

    Completed IRIS home page design

--- src/client/main/components/HomePage.jsx
+++ src/client/main/components/HomePage.jsx
@@ -4,39 +4,93 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => (
   <div id="iris-home-page">
-    <div className="iris-narrow-page">
-      <div style={{ width: '100%', height: '100px' }} />
-      <h1>A picture is worth a thousand words</h1>
-      <p>
-        Let's make IRIS worth ten thousand pictures.
-      </p>
-      <Link
-        className="iris-button action"
-        to="/login"
-      >
-        Login
-      </Link>
-      <Link
-        className="iris-button secondary"
-        to="/login"
-      >
-        Sign up to IRIS
-      </Link>
-      <div style={{ width: '100%', height: '200px' }} />
-      <h2>What is IRIS?</h2>
-      <div className="image-panel">
+    <div className="call-to-action">
+      <div className="iris-narrow-page">
+        <h1><em><q>A picture is worth a thousand words</q></em></h1>
         <p>
+          Let's make IRIS worth ten thousand pictures.
+        </p>
+        <Link
+          className="iris-button action"
+          to="/login"
+        >
+          Login
+        </Link>
+        <Link
+          className="iris-button secondary"
+          to="/create"
+        >
+          Sign up to IRIS
+        </Link>
+      </div>
+    </div>
+    <section aria-label="about IRIS" id="home-page-about-iris" className="iris-narrow-page">
+      <div className="column-info-panel three-column">
+        <h3 className="grid-header1">What is IRIS?</h3>
+        <p className="grid-content" data-index="1">
           IRIS is an application which connects blind and visually impaired students
           to a network of volunteers with experience in different subject areas.
         </p>
-      </div>
-      <div className="image-panel">
-        <p>
-          A student uploads an image from their course, and volunteers provide a
-          subject-specific description of that image.
+        <h3 className="grid-header2">Why is it useful?</h3>
+        <p className="grid-content2" data-index="2">
+          A student can upload an image from their course, and volunteers with
+          expertise in that subject area provide a subject-specific description
+          of that image.
+        </p>
+        <h3 className="grid-header3">What is the end result?</h3>
+        <p className="grid-content3" data-index="3">
+          The description is then sent to the student in an fully accessible form,
+          making sure nobody is left in the dark
         </p>
       </div>
-    </div>
+      <div className="spacer hline" />
+      <h2>How can we help?</h2>
+      <div className="spacer-small" />
+      <div className="column-info-panel two-column">
+        <h3 className="grid-header1">For visually impaired students</h3>
+        <p className="grid-content1">
+          IRIS is currently undergoing beta testing, meaning we are offering the
+          service for free while we work with our students and volunteers to
+          create the best possible experience.
+        </p>
+        <Link
+          to="/create/student"
+          className="iris-button tertiary grid-action1"
+        >
+            Sign up as a student
+        </Link>
+        <div className="spacer-small" />
+        <h3 className="grid-header2">For institutions</h3>
+        <p className="grid-content2">
+          We are keen to partner with schools, colleges and other places of
+          learning to provide multiple licences and ensure your students
+          receive the best education they can.
+        </p>
+        <Link
+          to="/create/licence-owner"
+          className="iris-button tertiary grid-action2"
+        >
+          Sign up as a licence-owner
+        </Link>
+      </div>
+      <div className="spacer hline" />
+      <section aria-label="call for volunteers" className="column-info-panel">
+        <h2>We need volunteers!</h2>
+        <p>
+          If you are a sighted volunteer who wishes to help describe images for
+          visually impaired students (and have a little experience with sciency-pictures)
+          we'd love to have you on board.
+        </p>
+        <Link
+          to="/create/volunteer"
+          className="iris-button action"
+        >
+            Sign up as a volunteer
+        </Link>
+      </section>
+      <div className="spacer-small" />
+    </section>
+    <div className="spacer" />
   </div>
 );
 
