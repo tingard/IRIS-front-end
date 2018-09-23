558fd3dfcd748e55e58b32d6fd0f40db40d08a17
Tim Lingard <tklingard@gmail.com>
2018-09-23 12:07:02 +0100

    Added IrisSelect component

---
+++ src/client/common-resources/IrisSelect.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const IrisSelect = (props) => {
+  let { id } = props;
+  if (id === false) {
+    id = Math.random();
+  }
+  return (
+    <label htmlFor={`${id}-select`} className="w3-panel">
+      {props.label}
+      <select
+        id={`${id}-select`}
+        className="iris-select"
+        onChange={e => props.onChange(e.target.value)}
+        value={props.value}
+      >
+        {props.options.map(
+          (option, i) => <option key={i} value={option.value}>{option.text}</option>,
+        )}
+      </select>
+    </label>
+  );
+};
+
+IrisSelect.propTypes = {
+  id: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  })).isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+IrisSelect.defaultProps = {
+  id: false,
+};
+
+export default IrisSelect;
