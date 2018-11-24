a5fd6357a4e99d65d43a858188d4ea6ac449865a
Tim Lingard <tklingard@gmail.com>
2018-11-24 11:55:30 +0000

    Added basic poi config

---
+++ poi.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: './src/client/main/index.jsx',
+    },
+    volunteer: {
+      entry: './src/client/volunteer/index.jsx',
+    },
+    student: {
+      entry: './src/client/student/index.jsx',
+    },
+    'licence-owner': {
+      entry: './src/client/licence-owner/index.jsx',
+    },
+  },
+};
