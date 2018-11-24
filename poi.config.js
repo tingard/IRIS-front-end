666d503eec70e4e42400202af13fa1adbad979ce
Tim Lingard <tklingard@gmail.com>
2018-11-24 15:59:46 +0000

    Restructuring files for poi

--- poi.config.js
+++ poi.config.js
@@ -3,14 +3,14 @@ module.exports = {
     index: {
       entry: './src/client/main/index.jsx',
     },
-    volunteer: {
-      entry: './src/client/volunteer/index.jsx',
-    },
-    student: {
-      entry: './src/client/student/index.jsx',
-    },
-    'licence-owner': {
-      entry: './src/client/licence-owner/index.jsx',
-    },
+    // volunteer: {
+    //   entry: './src/client/volunteer/index.jsx',
+    // },
+    // student: {
+    //   entry: './src/client/student/index.jsx',
+    // },
+    // 'licence-owner': {
+    //   entry: './src/client/licence-owner/index.jsx',
+    // },
   },
 };
