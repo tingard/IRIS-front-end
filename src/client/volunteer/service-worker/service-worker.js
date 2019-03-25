833f39406bb9d774c078062a97e7b814b51addf1
Tim Lingard <tklingard@gmail.com>
2019-03-25 13:29:18 +0000

    Cleaned-up service worker

--- src/client/volunteer/service-worker/service-worker.js
+++ src/client/volunteer/service-worker/service-worker.js
@@ -4,9 +4,6 @@ require('../../common-resources/offline-page-service-worker');
 const genNotificationFromStatus = require('./genNotificationFromStatus');
 
 async function sendStatus(status) {
-  // Exit early if we don't have access to the client.
-  // Eg, if it's cross-origin.
-  console.log('sending status');
   // Send a message to the client.
   self.clients.matchAll().catch(() => []).then(
     l => l.map(client => client.postMessage(status)),
@@ -17,11 +14,9 @@ self.addEventListener('push', (e) => {
   let msg;
   if (e.data) {
     msg = JSON.parse(e.data.text());
-    console.log(msg);
   } else {
     msg = { status: 'NO_STATUS', message: 'IRIS has new data!' };
   }
-  console.log(genNotificationFromStatus(msg)[1]);
   e.waitUntil(sendStatus(msg));
   e.waitUntil(
     self.registration.showNotification(...genNotificationFromStatus(msg)),
@@ -49,7 +44,7 @@ self.addEventListener('notificationclick', (e) => {
             clients[0].focus();
           }
         }
-      }).then(() => e.notification.close()),
+      }).then(e.notification.close),
     );
   } else {
     e.waitUntil(
@@ -60,5 +55,4 @@ self.addEventListener('notificationclick', (e) => {
       }).then(() => e.notification.close()),
     );
   }
-  // TODO close all notifications when one is clicked
 });
